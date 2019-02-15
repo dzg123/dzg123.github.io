a1e3fa44e51bfa158a02f434258eaedee17c3a10
dzg <1395224335@qq.com>
2019-02-15 15:53:21 +0800

    blog

--- js/main.js
+++ js/main.js
@@ -87,7 +87,7 @@ $(document).ready(function () {
 $(function () {
   if(location.pathname !=='/') return;
     console.log("href",location.href);
-  if(location.href !== 'dzg123.github.io') location.href = "dzg123.github.io";
+  if(location.href !== 'https://dzg123.github.io') location.href = "https://dzg123.github.io";
     var random = Math.floor(Math.random() * 5+1).toString();
   $.get("/data/shanbayToday"+random+".json", function (data) {
     var data = data.data;
