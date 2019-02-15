bda3057ee38924e4a060b50a2b8052f4762309f7
dzg <1395224335@qq.com>
2019-02-15 19:45:27 +0800

    blog

--- js/main.js
+++ js/main.js
@@ -1,114 +1,114 @@
 if (!!$.prototype.justifiedGallery) { // if justifiedGallery method is defined
-  var options = {
-    rowHeight: 140,
-    margins: 4,
-    lastRow: 'justify'
-  };
-  $('.article-gallery').justifiedGallery(options);
+    var options = {
+        rowHeight: 140,
+        margins: 4,
+        lastRow: 'justify'
+    };
+    $('.article-gallery').justifiedGallery(options);
 }
 
 $(document).ready(function () {
-  $("#menu > #nav").show();
-  $("#menu-icon, #menu-icon-tablet").click(function () {
-    if ($('#menu').css('visibility') == 'hidden') {
-      $('#menu').css('visibility', 'visible');
-      $('#menu-icon, #menu-icon-tablet').addClass('active');
+    $("#menu > #nav").show();
+    $("#menu-icon, #menu-icon-tablet").click(function () {
+        if ($('#menu').css('visibility') == 'hidden') {
+            $('#menu').css('visibility', 'visible');
+            $('#menu-icon, #menu-icon-tablet').addClass('active');
 
-      var topDistance = $("#menu > #nav").offset().top;
+            var topDistance = $("#menu > #nav").offset().top;
 
-      if ($('#menu').css('visibility') != 'hidden' && topDistance < 50) {
-        $("#menu > #nav").show();
-      } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
-        $("#menu > #nav").hide();
-      }
-      return false;
-    } else {
-      $('#menu').css('visibility', 'hidden');
-      $('#menu-icon, #menu-icon-tablet').removeClass('active');
-      return false;
+            if ($('#menu').css('visibility') != 'hidden' && topDistance < 50) {
+                $("#menu > #nav").show();
+            } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
+                $("#menu > #nav").hide();
+            }
+            return false;
+        } else {
+            $('#menu').css('visibility', 'hidden');
+            $('#menu-icon, #menu-icon-tablet').removeClass('active');
+            return false;
+        }
+    });
+
+    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+    $("#header > #nav > ul > .icon").click(function () {
+        $("#header > #nav > ul").toggleClass("responsive");
+    });
+
+    if ($("#menu").length) {
+        $(window).on('scroll', function () {
+            var topDistance = $("#menu > #nav").offset().top;
+
+            if ($('#menu').css('visibility') != 'hidden' && topDistance < 50) {
+                $("#menu > #nav").show();
+            } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
+                $("#menu > #nav").hide();
+            }
+
+            if (!$("#menu-icon").is(":visible") && topDistance < 50) {
+                $("#menu-icon-tablet").show();
+                $("#top-icon-tablet").hide();
+            } else if (!$("#menu-icon").is(":visible") && topDistance > 100) {
+                $("#menu-icon-tablet").hide();
+                $("#top-icon-tablet").show();
+            }
+        });
     }
-  });
 
-  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-  $("#header > #nav > ul > .icon").click(function () {
-    $("#header > #nav > ul").toggleClass("responsive");
-  });
+    if ($("#footer-post").length) {
+        var lastScrollTop = 0;
+        $(window).on('scroll', function () {
+            var topDistance = $(window).scrollTop();
 
-  if ($("#menu").length) {
-    $(window).on('scroll', function () {
-      var topDistance = $("#menu > #nav").offset().top;
+            if (topDistance > lastScrollTop) {
+                // downscroll code
+                $("#footer-post").hide();
+            } else {
+                // upscroll code
+                $("#footer-post").show();
+            }
+            lastScrollTop = topDistance;
 
-      if ($('#menu').css('visibility') != 'hidden' && topDistance < 50) {
-        $("#menu > #nav").show();
-      } else if ($('#menu').css('visibility') != 'hidden' && topDistance > 100) {
-        $("#menu > #nav").hide();
-      }
+            $("#nav-footer").hide();
+            $("#toc-footer").hide();
+            $("#share-footer").hide();
 
-      if (!$("#menu-icon").is(":visible") && topDistance < 50) {
-        $("#menu-icon-tablet").show();
-        $("#top-icon-tablet").hide();
-      } else if (!$("#menu-icon").is(":visible") && topDistance > 100) {
-        $("#menu-icon-tablet").hide();
-        $("#top-icon-tablet").show();
-      }
-    });
-  }
-
-  if ($("#footer-post").length) {
-    var lastScrollTop = 0;
-    $(window).on('scroll', function () {
-      var topDistance = $(window).scrollTop();
-
-      if (topDistance > lastScrollTop) {
-        // downscroll code
-        $("#footer-post").hide();
-      } else {
-        // upscroll code
-        $("#footer-post").show();
-      }
-      lastScrollTop = topDistance;
-
-      $("#nav-footer").hide();
-      $("#toc-footer").hide();
-      $("#share-footer").hide();
-
-      if (topDistance < 50) {
-        $("#actions-footer > ul > #top").hide();
-        $("#actions-footer > ul > #menu").show();
-      } else if (topDistance > 100) {
-        $("#actions-footer > ul > #menu").hide();
-        $("#actions-footer > ul > #top").show();
-      }
-    });
-  }
+            if (topDistance < 50) {
+                $("#actions-footer > ul > #top").hide();
+                $("#actions-footer > ul > #menu").show();
+            } else if (topDistance > 100) {
+                $("#actions-footer > ul > #menu").hide();
+                $("#actions-footer > ul > #top").show();
+            }
+        });
+    }
 });
 
 
 $(function () {
-  if(location.pathname !=='/') return;
-    var random = Math.floor(Math.random() * 5+1).toString();
-  $.get("/data/shanbayToday"+random+".json", function (data) {
-    var data = data.data;
-    // var str =  data.content+'\n'
-    // + data.translation+"\n---- "
-    // +data.author +'\n'
-    var str =  data.content+'\n'
-    + data.translation+"\n---- "
-    
-    var options = {
-      strings: [
-		str + "Hello,my friends^1000",
-        str + "i'm dzg ^3000",
-		   str+"Welcome to my blog└(^o^)┘Y(^_^)Y\(^o^)/. ^1000",
-        str +data.author,
-      
-      ],
-      typeSpeed: 20,
-      startDelay:300,
-      // loop: true,
-    }
-    var typed = new Typed(".description .typed", options);
-  })
+    if (location.pathname !== '/') return;
+    var random = Math.floor(Math.random() * 5 + 1).toString();
+    $.get("/data/shanbayToday" + random + ".json", function (data) {
+        var data = data.data;
+        // var str =  data.content+'\n'
+        // + data.translation+"\n---- "
+        // +data.author +'\n'
+        var str = data.content + '\n'
+            + data.translation + "\n---- "
+
+        var options = {
+            strings: [
+                str + "Hello,my friends^1000",
+                str + "i'm dzg ^3000",
+                str + "Welcome to my blog└(^o^)┘Y(^_^)Y\(^o^)/. ^1000",
+                str + data.author,
+
+            ],
+            typeSpeed: 20,
+            startDelay: 300,
+            // loop: true,
+        }
+        var typed = new Typed(".description .typed", options);
+    })
 });
 
 // var _mtac = {};
@@ -132,21 +132,21 @@ $(function () {
 // });
 
 //  toc 里面是a标签问题
-$(function(){
-    if(location.href !== 'https://dzg123.github.io/') location.href = "https://dzg123.github.io/";
-  if($("#toc li a")){
-    $("#toc a").each(function(index){
-      var ele = $(this);
-      if(ele.attr('href').indexOf('http') != -1){
-        ele.attr('href',ele.prev().attr('href'))
-      }
-    })
-  
-    $("#TableOfContents ul").children().each(function(index){
-      if($(this).children('a').length===0){
-        $(this).addClass('no-before')
-      }
-    })
-  
-  }
+$(function () {
+    if (location.href.indexOf('dzg123') === -1) location.href = "https://dzg123.github.io/";
+    if ($("#toc li a")) {
+        $("#toc a").each(function (index) {
+            var ele = $(this);
+            if (ele.attr('href').indexOf('http') != -1) {
+                ele.attr('href', ele.prev().attr('href'))
+            }
+        })
+
+        $("#TableOfContents ul").children().each(function (index) {
+            if ($(this).children('a').length === 0) {
+                $(this).addClass('no-before')
+            }
+        })
+
+    }
 })
